c864c138ad1f2e1e17aef5cd2b304db07f72a4d3
Anush Indrajith <anushcs55@gmail.com>
2018-09-19 00:32:43 +0530

    user scema

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const config = require('./config/database');
+
+const connection = mongoose.connect(config.database);
+if (connection){
+    console.log('database connected');
+}else{
+    console.log('database not connected');
+}
+
 const user = require('./routes/users');
 
 app.use(express.static(path.join(__dirname,"public")));
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../modals/user');
+
 
 router.get("/",function(req,res){
     res.send("Hello worlds");
     });
+
+    router.post("/register",function(req,res){
+        const newUser = new User({
+            name:req.body.name,
+            email:req.body.email,
+            type:req.body.type,
+            password:req.body.password
+                
+        });
+        });
 
 module.exports = router;
